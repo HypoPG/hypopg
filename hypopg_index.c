8e43592e6a1de77babdbac9a61d541de4e5ac29d
Godwottery <the_mix_room@hotmail.com>
2019-01-19 12:47:34 +0100

    Add explicit type-cast so silence compiler warnings.

--- hypopg_index.c
+++ hypopg_index.c
@@ -1846,8 +1846,7 @@ hypo_estimate_index(hypoIndex *entry, RelOptInfo *rel, PlannerInfo *root)
 			  - (fillfactor == 0 ? BTREE_DEFAULT_FILLFACTOR : fillfactor)
 						+ additional_bloat) / 100;
 
-		entry->pages =
-			entry->tuples * line_size * bloat_factor / usable_page_size;
+		entry->pages = (BlockNumber) (entry->tuples * line_size * bloat_factor / usable_page_size);
 #if PG_VERSION_NUM >= 90300
 		entry->tree_height = -1;	/* TODO */
 #endif
@@ -1929,7 +1928,7 @@ hypo_estimate_index(hypoIndex *entry, RelOptInfo *rel, PlannerInfo *root)
 			sizeof_SignType * bloomLength;
 
 		entry->pages = 1; /* meta page */
-		entry->pages += ceil(
+		entry->pages += (BlockNumber) ceil(
 				((double) entry->tuples * line_size) / usable_page_size);
 	}
 #endif
